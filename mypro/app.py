1feeb569a60075bfbe8e2239bfde96b54df97735
Yavuz <64099848+YVZYLDZ11@users.noreply.github.com>
2023-07-22 01:28:17 +0300

    Update app.py

--- mypro/app.py
+++ mypro/app.py
@@ -52,3 +52,32 @@ def hello_python():
 if __name__ == '__main__':
    app.run()
 
+
+#127.0.0.1:5000/admin
+##127.0.0.1:5000/user/mvl
+
+from flask import Flask, redirect, url_for
+app = Flask(__name__)
+
+@app.route('/admin')
+def hello_admin():
+   return 'Hello Admin'
+
+@app.route('/guest/<guest>')
+def hello_guest(guest):
+   return 'Hello %s as Guest' % guest
+
+@app.route('/user/<name>')
+def hello_user(name):
+   if name =='admin':
+      return redirect(url_for('hello_admin'))
+   else:
+      return redirect(url_for('hello_guest',guest = name))
+
+if __name__ == '__main__':
+   app.run(debug = True)
+   
+
+
+
+
