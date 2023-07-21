46260fc1bc340c8b67fa54d19a1aa522dc223f80
Yavuz <64099848+YVZYLDZ11@users.noreply.github.com>
2023-07-21 16:15:06 +0300

    Update app.py

--- mypro/app.py
+++ mypro/app.py
@@ -35,3 +35,20 @@ def revision(revNo):
 
 if __name__ == '__main__':
    app.run()
+
+# /python/ > Hello Python
+# /flask/ > 404 not found
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/flask')
+def hello_flask():
+   return 'Hello Flask'
+
+@app.route('/python/')
+def hello_python():
+   return 'Hello Python'
+
+if __name__ == '__main__':
+   app.run()
+
