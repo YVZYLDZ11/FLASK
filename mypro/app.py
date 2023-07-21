7d619f90c9fbde4c443fd1935cf518b136b6e132
Yavuz <64099848+YVZYLDZ11@users.noreply.github.com>
2023-07-21 16:10:27 +0300

    Update app.py

--- mypro/app.py
+++ mypro/app.py
@@ -19,3 +19,19 @@ def hello_name(name):
 
 if __name__ == '__main__':
    app.run(debug = True)
+
+#127.0.0.1:5000/blog/11
+
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/blog/<int:postID>')
+def show_blog(postID):
+   return 'Blog Number %d' % postID
+
+@app.route('/rev/<float:revNo>')
+def revision(revNo):
+   return 'Revision Number %f' % revNo
+
+if __name__ == '__main__':
+   app.run()
